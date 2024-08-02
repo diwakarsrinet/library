1b205205821195d6fd473572e3423a783c343eb1
diwakarsrinet <169426962+diwakarsrinet@users.noreply.github.com>
2024-08-02 21:56:56 +0530

    Final app with all features completed

--- script.js
+++ script.js
@@ -21,11 +21,12 @@ addButton.addEventListener("click", () => {
 const saveBookForm = document.getElementById("newBookForm");
 saveBookForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    const entryTitle = document.getElementById("entryBookTitle");
-    const entryAuthor = document.getElementById("entryBookAuthor");
-    const entryPages = document.getElementById("entryBookPages");
-    const entryRead = document.getElementById("isEntryRead");
-    addBookToLibrary(new Book(entryTitle.textContent,entryAuthor.textContent,entryPages.textContent,(entryRead.checked?"Read":"Not read")));
+    let entryTitle = document.getElementById("entryBookTitle").value;
+    let entryAuthor = document.getElementById("entryBookAuthor").value;
+    let entryPages = document.getElementById("entryBookPages").value;
+    let entryRead = document.getElementById("isEntryRead");
+    addBookToLibrary(new Book(entryTitle,entryAuthor,entryPages,(entryRead.checked?"Read":"Not read")));
+    displayBook(myLibrary.length-1);
     saveBookForm.reset();
 });
 
@@ -39,33 +40,54 @@ closeForm.addEventListener("click", (e) => {
 // Display current book list
 function displayBook() {
     const booksCollection = document.getElementById("books-container")
-    for (i=0; i<myLibrary.length; i++) {
-        let newBook = document.createElement("div");
-        newBook.className = "cards";
-        booksCollection.appendChild(newBook);
-        let newTitle = document.createElement("p");
-        newTitle.className = "bookTitle";
-        newTitle.innerHTML = myLibrary[i].title;
-        let newAuthor = document.createElement("p");
-        newAuthor.className = "bookAuthor";
-        newAuthor.innerHTML = "By " + myLibrary[i].author;
-        let newPages = document.createElement("p");
-        newPages.className = "bookPages";
-        newPages.innerHTML = myLibrary[i].pages + " pages";
-        let newStatus = document.createElement("p");
-        newStatus.className = "bookReadStatus";
-        newStatus.innerHTML = myLibrary[i].isRead;
-        let deleteButton = document.createElement("button");
-        deleteButton.className = "bookDeleteButton";
-        deleteButton.innerHTML = "Delete";
-        newBook.appendChild(newTitle);
-        newBook.appendChild(newAuthor);
-        newBook.appendChild(newPages);
-        newBook.appendChild(newStatus);
-        newBook.appendChild(deleteButton);
-    }
+    let newBook = document.createElement("div");
+    newBook.className = "cards";
+    booksCollection.appendChild(newBook);
+    let newTitle = document.createElement("p");
+    newTitle.className = "bookTitle";
+    newTitle.innerHTML = myLibrary[i].title;
+    let newAuthor = document.createElement("p");
+    newAuthor.className = "bookAuthor";
+    newAuthor.innerHTML = "By " + myLibrary[i].author;
+    let newPages = document.createElement("p");
+    newPages.className = "bookPages";
+    newPages.innerHTML = myLibrary[i].pages + " pages";
+    let newStatus = document.createElement("p");
+    newStatus.className = "bookReadStatus";
+    newStatus.innerHTML = myLibrary[i].isRead;
+    let readToggle = document.createElement("img");
+    readToggle.className = "changeReadStatus";
+    readToggle.src = "./assets/read.png";
+    readToggle.alt = "change read status";
+    readToggle.width = 32;
+    readToggle.height = 32;
+    let deleteButton = document.createElement("img");
+    deleteButton.className = "bookDeleteButton";
+    deleteButton.src = "./assets/trash-can.png";
+    deleteButton.alt = "delete book";
+    deleteButton.width = 32;
+    deleteButton.height = 32;
+    newBook.appendChild(newTitle);
+    newBook.appendChild(newAuthor);
+    newBook.appendChild(newPages);
+    newBook.appendChild(newStatus);
+    newBook.appendChild(readToggle);
+    newBook.appendChild(deleteButton);
+
+// Delete book
+    deleteButton.addEventListener("click", () => {
+        newBook.remove();
+    });
+
+// Change read status
+    readToggle.addEventListener("click", () => {
+        if  (newStatus.innerHTML === "Read") newStatus.innerHTML = "Not read";
+        else newStatus.innerHTML = "Read";
+    });
 }
 
+
+// Adding some example books
 addBookToLibrary(new Book("Ramayana","Valmiki","900","Read"));
 addBookToLibrary(new Book("Meghdoot","Kalidasa","108", "Not read"));
 addBookToLibrary(new Book("Rashmirathi","Ramdhari Singh Dinkar","191", "Not read"));
@@ -74,4 +96,6 @@ addBookToLibrary(new Book("The Immortals of Meluha","Amish Tripathi","415", "Rea
 addBookToLibrary(new Book("Rashmirathi","Ramdhari Singh Dinkar","191", "Not read"));
 
 
-document.addEventListener("DOMContentLoaded", displayBook);
+for (i=0; i<myLibrary.length; i++) {
+    displayBook();
+}
